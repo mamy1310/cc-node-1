45216bc152dea1c2e809f025edb07e36c0259425
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-06-22 14:38:36 +0200

    Extract express file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-const express = require('express');
-const app = express();
+const app = require("./app.js");
+
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
