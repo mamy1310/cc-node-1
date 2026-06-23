d84969323cbba36ecbb06ca3ae37ad465ff087f7
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-06-23 15:18:37 +0200

    Get all woods

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,10 @@
-export const getAll = (req, res) => {
-	res.send("List of woods");
+import { prisma } from "../../app.js";
+
+export const getAll = async (req, res) => {
+	try {
+		const woods = await prisma.wood.findMany();
+		res.json(woods);
+	} catch (error) {
+		res.status(500).json(error.message || "Error on getting all woods");
+	}
 };
