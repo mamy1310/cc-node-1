b25ce7f2325281031dc489e5f6e17253cef188f7
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-06-23 15:36:55 +0200

    Find woods by hardness

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -5,6 +5,17 @@ export const getAll = async (req, res) => {
 		const woods = await prisma.wood.findMany();
 		res.json(woods);
 	} catch (error) {
-		res.status(500).json(error.message || "Error on getting all woods");
+		res.status(error.status || 500).json(error.message || "Error on getting all woods");
+	}
+};
+
+export const readByHardness = async (req, res) => {
+	try {
+		const woods = await prisma.wood.findMany({
+			where: { hardness: req.params.hardness },
+		});
+		res.json(woods);
+	} catch (error) {
+		res.status(error.status || 500).json(error.message || "Error on getting woods by hardness");
 	}
 };
--- app/routes/wood.js
+++ app/routes/wood.js
@@ -4,5 +4,6 @@ import * as woodController from "../controllers/wood.js";
 const router = express.Router();
 
 router.get("/", woodController.getAll);
+router.get("/:hardness", woodController.readByHardness);
 
 export default router;
