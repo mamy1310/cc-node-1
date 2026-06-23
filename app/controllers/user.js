f6a6b1d09498e33fd872c9c7052b092bf2e40e19
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-06-23 14:52:54 +0200

    Signup

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,14 @@
-export const signup = (req, res) => {
-	res.send("You are signup");
+import { prisma } from "../../app.js";
+
+export const signup = async (req, res) => {
+	try {
+		const user = await prisma.user.create({
+			data: req.body,
+		});
+		res.json(user);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
 
 export const login = (req, res) => {
