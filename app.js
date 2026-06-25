4a94cb1917f444cfe2f483c779ddf4383124584a
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-06-25 14:15:41 +0200

    Add swagger doc

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from "./swagger.json" with { type: "json" };
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import router from "./app/routes/index.js";
@@ -8,6 +10,7 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:8080' }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const databaseUrl = new URL(process.env.DATABASE_URL);
 
